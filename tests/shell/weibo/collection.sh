77b1a0eaaf96a78af51d93c474d7cee5c08885b7
markhuyong <markhuyong@gmail.com>
2017-03-09 09:52:00 +0800

    调整weibo 爬虫

--- tests/shell/weibo/collection.sh
+++
@@ -1,1 +0,0 @@
-curl "http://localhost:9080/crawl.json?spider_name=collection&url=http://www.jianshu.com/c/f0272b32bc8c"
---
+++ tests/shell/weibo/status.sh
@@ -0,0 +1,1 @@
+curl "http://localhost:9080/crawl.json?spider_name=weibo_status&uid=5903871159"
