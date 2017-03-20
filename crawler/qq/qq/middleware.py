79d6495eed5ab1af0c206a74fd87225a701f5d8b
markhuyong <markhuyong@gmail.com>
2017-03-20 19:42:50 +0800

    fix qq cookies error

--- crawler/qq/qq/middleware.py
+++ crawler/qq/qq/middleware.py
@@ -10,21 +10,7 @@ from scrapy.exceptions import IgnoreRequest
 from scrapy.utils.response import response_status_message
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
 
-# from logging.config import fileConfig
-
-# fileConfig('logging_config.ini')
 from crawler.qq.qq.utils import BaseHelper
-
-logger = logging.getLogger(__name__)
-
-
-# handler = logging.StreamHandler()
-# handler = logging.FileHandler('hello.log')
-# formatter = logging.Formatter(
-#         '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
-# logger.setLevel(logging.DEBUG)
 
 
 class CookiesMiddleware(RetryMiddleware):
@@ -44,21 +30,24 @@ class CookiesMiddleware(RetryMiddleware):
         return cls(crawler.settings, crawler)
 
     def process_request(self, request, spider):
-        logger.debug("cookies request.url ======{}".format(request.url))
+        spider.logger.debug("cookies request.url ======{}".format(request.url))
         prefix = BaseHelper.get_cookie_key_prefix(spider)
         redisKeys = self.rconn.keys("{}:*".format(prefix))
         while len(redisKeys) > 0:
             elem = random.choice(redisKeys)
             if prefix in elem:
                 cookie = json.loads(self.rconn.get(elem))
-                logger.info("cookies request.url ======{}".format(request.url))
+                spider.logger.info("cookies request.url ======{}".format(request.url))
+                spider.logger.info(
+                    "cookies request.url ======{}".format(cookie))
+
 
                 new_url = self._replace_token(request.url, cookie)
                 if not request.url == new_url:
                     # FIXME: hack protected method
                     request._set_url(new_url)
 
-                logger.debug(
+                    spider.logger.debug(
                     "request.replace(url=new_url) request url ======{}".format(
                         request.url))
                 request.cookies = cookie
@@ -73,15 +62,15 @@ class CookiesMiddleware(RetryMiddleware):
             try:
                 redirect_url = response.headers["location"]
                 if "login.weibo" in redirect_url or "login.sina" in redirect_url:  # Cookie失效
-                    logger.warning("One Cookie need to be updating...")
+                    spider.logger.warning("One Cookie need to be updating...")
                     updateCookie(request.meta['accountText'], self.rconn,
                                  spider)
                 elif "weibo.cn/security" in redirect_url:  # 账号被限
-                    logger.warning("One Account is locked! Remove it!")
+                    spider.logger.warning("One Account is locked! Remove it!")
                     removeCookie(request.meta["accountText"], self.rconn,
                                  spider)
                 elif "weibo.cn/pub" in redirect_url:
-                    logger.warning(
+                    spider.logger.warning(
                         "Redirect to 'http://weibo.cn/pub'!( Account:%s )" %
                         request.meta["accountText"].split("--")[0])
                 reason = response_status_message(response.status)
@@ -89,12 +78,12 @@ class CookiesMiddleware(RetryMiddleware):
             except Exception, e:
                 raise IgnoreRequest
         elif response.status in [403, 414]:
-            logger.error("%s! Stopping..." % response.status)
+            spider.logger.error("%s! Stopping..." % response.status)
             reason = response_status_message(response.status)
             return self._retry(request, reason, spider) or response
             # os.system("pause")
         elif u'登录态失效，请重新登录' in response.body:
-            logger.warning("One Cookie need to be updating...")
+            spider.logger.warning("One Cookie need to be updating...")
             updateCookie(request.meta['accountText'], self.rconn,
                          spider)
             reason = response_status_message(response.status)
@@ -105,19 +94,29 @@ class CookiesMiddleware(RetryMiddleware):
     @classmethod
     def _replace_token(cls, url, cookies):
         if "GTK" in url:
-            p_skey = None
-            skey = None
-            rv2 = None
-            for cookie in cookies:
-                if "p_skey" in cookie['name']:
-                    p_skey = cookie['value']
-                if "skey" == cookie['name']:
-                    skey = cookie['value']
-                if "rv2" == cookie['name']:
-                    rv2 = cookie['value']
+            p_skey = cookies.get("p_skey")
+            skey = cookies.get("skey")
+            rv2 = cookies.get("rv2")
             g_tk = cls.getNewGTK(p_skey, skey, rv2)
             url = url.replace("GTK", g_tk)
         return url
+    #
+    # @classmethod
+    # def _replace_token(cls, url, cookies):
+    #     if "GTK" in url:
+    #         p_skey = None
+    #         skey = None
+    #         rv2 = None
+    #         for cookie in cookies:
+    #             if "p_skey" in cookie['name']:
+    #                 p_skey = cookie['value']
+    #             if "skey" == cookie['name']:
+    #                 skey = cookie['value']
+    #             if "rv2" == cookie['name']:
+    #                 rv2 = cookie['value']
+    #         g_tk = cls.getNewGTK(p_skey, skey, rv2)
+    #         url = url.replace("GTK", g_tk)
+    #     return url
 
     # @classmethod
     # def _replace_token(cls, url, cookies):
