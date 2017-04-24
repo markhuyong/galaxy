ae440b84f47d06c83189930a8bff49da278bc09c
markhuyong <markhuyong@gmail.com>
2017-04-24 18:02:10 +0800

    add nohup to start shell

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-python run.py -c jianshu -p 9080 -i 0.0.0.0 &
-python run.py -c qq -p 9081 -i 0.0.0.0 &
-python run.py -c weibo -p 9082 -i 0.0.0.0 &
+nohup python run.py -c jianshu -p 9080 -i 0.0.0.0 &
+nohup python run.py -c qq -p 9081 -i 0.0.0.0 &
+nohup python run.py -c weibo -p 9082 -i 0.0.0.0 &
